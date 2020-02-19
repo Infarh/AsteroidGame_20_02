e342878ed0cf7ec10ed73501dc95a12ef3049eac
Infarh <shmachilin@yandex.ru>
2020-02-19 20:16:13 +0300

    Рефакторинг

---
+++ Tests/TestConsole/Gamer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace TestConsole
+{
+    class Gamer
+    {
+        private string _Name;
+        private DateTime _DayOfBirth;
+
+        public string Name
+        {
+            get
+            {
+                //return _Name ?? string.Empty;
+                //return _Name == null ? string.Empty : _Name;
+                if (_Name == null)
+                    return string.Empty;
+                else
+                    return _Name;
+            }
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException(nameof(value));
+                _Name = value;
+            }
+        }
+
+        public Gamer(string Name, DateTime DayOfBirth)
+        {
+            _Name = Name;
+            _DayOfBirth = DayOfBirth;
+        }
+
+        //public void SetName(string value)
+        //{
+        //    _Name = value;
+        //}
+
+        //public string GetName()
+        //{
+        //    return _Name;
+        //}
+
+        internal int GetNameLength()
+        {
+            return Name.Length;
+        }
+
+        public void SayYourName()
+        {
+            Console.WriteLine("My name is {0} - {1:dd:MM:yyyy HH:mm:ss}", _Name, _DayOfBirth);
+        }
+    }
+}
--- Tests/TestConsole/Program.cs
+++ Tests/TestConsole/Program.cs
@@ -8,29 +8,37 @@ namespace TestConsole
 {
     static class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args)   // Ctrl+K,D
         {
-            Gamer gamer = new Gamer();
-            gamer.Name = "Игрок 1";
-            gamer.DayOfBirth = new DateTime(1983, 12, 5, 12, 23, 17);
+            Gamer gamer = new Gamer("Игрок 1", new DateTime(1983, 12, 5, 12, 23, 17));
 
             Gamer[] gamers = new Gamer[100];
             for (var i = 0; i < gamers.Length; i++)
             {
-                var g = new Gamer();
+                var g = new Gamer(string.Format("Gamer {0}", i + 1), DateTime.Now.Subtract(TimeSpan.FromDays(365 * (i + 18))));
                 //g.Name = $"Gamer {i + 1}";
-                g.Name = string.Format("Gamer {0}", i + 1);
-                g.DayOfBirth = DateTime.Now;
                 gamers[i] = g;
             }
 
+            gamer.SayYourName();
+
+            Console.WriteLine();
+
+            foreach (var g in gamers)
+                g.SayYourName();
+
+            Console.WriteLine();
+
+            //gamer.SetName("2123");
+            //Console.WriteLine("Имя игрока {0}", gamer.GetName());
+
+            gamer.Name = "123";
+
+            //gamer.GetNameLength();
+
+            Console.WriteLine("Игрок {0}", gamer.Name);
+
             Console.ReadLine();
         }
-    }
-
-    class Gamer
-    {
-        public string Name;
-        public DateTime DayOfBirth;
     }
 }
