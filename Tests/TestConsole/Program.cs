09cde48e0cd9f2ba01fa758c4798f122bfef9ca0
Infarh <shmachilin@yandex.ru>
2020-02-19 19:47:05 +0300

    Игроки...

--- Tests/TestConsole/Program.cs
+++ Tests/TestConsole/Program.cs
@@ -6,10 +6,31 @@ using System.Threading.Tasks;
 
 namespace TestConsole
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
+            Gamer gamer = new Gamer();
+            gamer.Name = "Игрок 1";
+            gamer.DayOfBirth = new DateTime(1983, 12, 5, 12, 23, 17);
+
+            Gamer[] gamers = new Gamer[100];
+            for (var i = 0; i < gamers.Length; i++)
+            {
+                var g = new Gamer();
+                //g.Name = $"Gamer {i + 1}";
+                g.Name = string.Format("Gamer {0}", i + 1);
+                g.DayOfBirth = DateTime.Now;
+                gamers[i] = g;
+            }
+
+            Console.ReadLine();
         }
+    }
+
+    class Gamer
+    {
+        public string Name;
+        public DateTime DayOfBirth;
     }
 }
