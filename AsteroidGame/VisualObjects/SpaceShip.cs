2ec4ca355a69c7a8823f6abeb415885645628d96
Infarh <shmachilin@yandex.ru>
2020-02-26 21:47:26 +0300

    Добавляем событие разрушения корабля в игру, а также систему его управления с клавиатуры

--- AsteroidGame/VisualObjects/SpaceShip.cs
+++ AsteroidGame/VisualObjects/SpaceShip.cs
@@ -4,11 +4,18 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AsteroidGame.VisualObjects.Interfaces;
 
 namespace AsteroidGame.VisualObjects
 {
-    public class SpaceShip : VisualObject
+    public class SpaceShip : VisualObject, ICollision
     {
+        public event EventHandler ShipDestroyed;
+
+        private int _Energy = 20;
+
+        public int Energy => _Energy;
+
         public SpaceShip(Point Position, Point Direction, Size Size) 
             : base(Position, Direction, Size)
         {
@@ -16,7 +23,41 @@ namespace AsteroidGame.VisualObjects
 
         public override void Draw(Graphics g)
         {
-            throw new NotImplementedException();
+            var rect = Rect;
+            g.FillEllipse(Brushes.Blue, rect);
+            g.DrawEllipse(Pens.Yellow, rect);
+        }
+
+        public override void Update() {  }
+
+        public void ChangeEnergy(int delta)
+        {
+            _Energy += delta;
+            if (_Energy < 0)
+                ShipDestroyed?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void MoveUp()
+        {
+            if(_Position.Y > 0)
+                _Position = new Point(_Position.X, _Position.Y - _Direction.Y);
+        }
+
+        public void MoveDown()
+        {
+            if (_Position.Y - _Size.Height < Game.Height)
+              _Position = new Point(_Position.X, _Position.Y + _Direction.Y);
+        }
+
+        public bool CheckCollision(ICollision obj)
+        {
+            var is_collision = Rect.IntersectsWith(obj.Rect);
+            if (is_collision && obj is Asteroid asteroid)
+            {
+                ChangeEnergy(-asteroid.Power);
+            }
+
+            return is_collision;
         }
     }
 }
