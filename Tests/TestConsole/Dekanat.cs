4cb277cd022e6489f57da7f6c2f316153f86bfc9
Infarh <shmachilin@yandex.ru>
2020-02-29 17:18:06 +0300

    Примеры работы со списком, стеком и очередью

---
+++ Tests/TestConsole/Dekanat.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace TestConsole
+{
+    class Dekanat : Storage<Student>
+    {
+        public event Action<Student> ExelentStudentAdded;
+
+        public Dekanat()
+        {
+            NewItemAdded += OnNewItemAdded;
+        }
+
+        private void OnNewItemAdded(Student student)
+        {
+            if(student.AverageRating > 4.5)
+                ExelentStudentAdded?.Invoke(student);
+        }
+
+        public override void SaveToFile(string FileName)
+        {
+            //using(var file_stream = new FileStream(FileName, FileMode.Create, FileAccess.Write, FileShare.None))
+            //using (var writer = new StreamWriter(file_stream))
+            //{
+            //    // Работа с файлом на запись
+            //}
+
+            using (var file_writer = File.CreateText(FileName))
+                foreach (var student in _Items)
+                {
+                    file_writer.Write(student.Name);
+                    if (student.Ratings.Count > 0)
+                    {
+                        var ratings_string = string.Join(",", student.Ratings);
+                        file_writer.Write($",{ratings_string}");
+                    }
+
+                    file_writer.WriteLine();
+                }
+        }
+
+        public override void LoadFromFile(string FileName)
+        {
+            //if(!File.Exists(FileName)) return;
+            if(!File.Exists(FileName))
+                throw new FileNotFoundException("Файл с данными деканата не найден", FileName);
+
+            base.LoadFromFile(FileName);
+
+            //using (var file_stream = new FileStream(FileName, FileMode.Open, FileAccess.Read, FileShare.Read))
+            //using (var buffer_stream = new BufferedStream(file_stream, 1024 * 1024))
+            //using (var reader = new StreamReader(buffer_stream))
+            //{
+
+            //}
+
+            using (var file_reader = File.OpenText(FileName))
+            {
+                while (!file_reader.EndOfStream)
+                {
+                    var str = file_reader.ReadLine();
+                    if (string.IsNullOrWhiteSpace(str)) continue;
+
+                    var data_elements = str.Split(',');
+                    if(data_elements.Length == 0) continue;
+                    
+                    var student = new Student { Name = data_elements[0] };
+                    if (data_elements.Length > 1)
+                    {
+                        var ratings = new List<int>();
+                        for(var i = 1; i < data_elements.Length; i++)
+                            ratings.Add(int.Parse(data_elements[i]));
+                        student.Ratings = ratings;
+                    }
+
+                    Add(student);
+                }
+            }
+        }
+    }
+}
--- Tests/TestConsole/Program.cs
+++ Tests/TestConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -8,7 +9,79 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
+            //var simple_array_list = new ArrayList();
+
+            //simple_array_list.Add(42);
+            //simple_array_list.Add(new object());
+            //simple_array_list.Add(3.14159265358979);
+            //simple_array_list.Add("Hello World!");
+
+            //for (var i = 0; i < simple_array_list.Count; i++)
+            //{
+            //    var value = simple_array_list[i];
+            //    if (value is int)
+            //    {
+            //        int v = (int) value;
+            //        Console.WriteLine("Int:{0}", v);
+            //    }
+            //    else if (value is string)
+            //    {
+            //        string v = (string)value;
+            //        Console.WriteLine("Str:{0}", v);
+            //    }
+            //    else if (value is double)
+            //    {
+            //        double v = (double)value;
+            //        Console.WriteLine("double:{0}", v);
+            //    }
+            //}
+
+            //System.Collections.Generic.
+
+            List<Student> students = new List<Student>(45);
+            for (var i = 0; i < 46; i++)
+                students.Add(new Student());
+
+            var students_to_add = new Student[20];
+            for (var i = 0; i < students_to_add.Length; i++)
+                students_to_add[i] = new Student();
+            students.AddRange(students_to_add);
+
+            students.Capacity = students.Count;
+
+            var new_students_list = new List<Student>(students_to_add);
+            students.RemoveAt(5);
+
+            var numbers_list = new List<int>(1000);
+            for(var i = 0; i < numbers_list.Capacity; i++)
+                numbers_list.Add(i + 72);
+            var value_index = numbers_list.BinarySearch(712);
+
+            var strings_list = new List<string>(1000);
+            for (var i = 0; i < strings_list.Capacity; i++)
+                strings_list.Add($"Message {i + 21}");
+            strings_list.Sort((s1, s2) => StringComparer.Ordinal.Compare(s2, s1));
+
+            //var strings_array = strings_list.ToArray();
+            //var strings_array = new string[strings_list.Count];
+            //strings_list.CopyTo(strings_array, 0);
+
+            var str_value_index = strings_list.BinarySearch("Message 712");
+            //strings_list[5] = "qwe";
+
+            //strings_list.ForEach(PrintString);
+
+            const string data_dir_name = "TestData";
+
+            //var total_lines = DataDirectoryProcessor.GetTotalLinesCountStack(data_dir_name);
+            //var total_lines = DataDirectoryProcessor.GetTotalLinesCountQueue(data_dir_name);
+            //Console.WriteLine("Число строк = {0}", total_lines);
+
+            
+
             Console.ReadLine();
         }
+
+        private static void PrintString(string str) => Console.WriteLine("Str = " + str);
     }
 }
