0aa6b5ea44c02c7ca76ae415c798570d26dbffbf
Infarh <shmachilin@yandex.ru>
2020-03-07 18:59:41 +0300

    Синтаксический сахар

--- MVVMTesting/MVVMTestApp/ViewModels/Base/ViewModel.cs
+++ MVVMTesting/MVVMTestApp/ViewModels/Base/ViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,10 +12,10 @@ namespace MVVMTestApp.ViewModels.Base
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
-        protected virtual void OnPropertyChanged(string PropertyName) =>
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null) =>
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
 
-        protected virtual bool Set<T>(ref T field, T value, string PropertyName)
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
         {
             if (Equals(field, value)) return false;
             field = value;
---
+++ MVVMTesting/MVVMTestApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Timers;
+
+namespace MVVMTestApp.ViewModels
+{
+    class MainWindowViewModel
+    {
+        private Timer _Timer;
+
+        public string Title { get; set; } = "Заголовок окна проекта MVVM";
+
+        public DateTime CurrentTime { get; set; }
+
+        public MainWindowViewModel()
+        {
+            _Timer = new Timer(100) { AutoReset = true };
+            _Timer.Elapsed += OnTimerTick;
+            _Timer.Start();
+        }
+
+        private void OnTimerTick(object Sender, ElapsedEventArgs E)
+        {
+            CurrentTime = DateTime.Now;
+        }
+    }
+}
