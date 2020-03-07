75e66fbb97a1ef1800a17f759a03784366a10119
Infarh <shmachilin@yandex.ru>
2020-03-07 18:59:08 +0300

    Базовая раелазиция модели-представления

---
+++ MVVMTesting/MVVMTestApp/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMTestApp.ViewModels.Base
+{
+    abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string PropertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+
+        protected virtual bool Set<T>(ref T field, T value, string PropertyName)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+    }
+}
