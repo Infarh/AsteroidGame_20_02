b279676699377ec12eb0cdd5033c63238d142f04
Infarh <shmachilin@yandex.ru>
2020-03-07 17:21:12 +0300

    Привязки

---
+++ BindingTests/BindingTestsApp/Mainwindow2.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace BindingTestsApp
+{
+    public partial class MainWindow2
+    {
+        public MainWindow2()
+        {
+            InitializeComponent();
+
+            var horizontal_binding = new Binding();
+            horizontal_binding.ElementName = "HorizontalSlider";
+            horizontal_binding.Path = new PropertyPath("Value");
+
+            HorizontalValue.SetBinding(TextBlock.TextProperty, horizontal_binding);
+
+            var vertival_binding = new Binding();
+            vertival_binding.ElementName = "VerticalSlider";
+            vertival_binding.Path = new PropertyPath("Value");
+
+            VerticalValue.SetBinding(TextBlock.TextProperty, vertival_binding);
+        }
+    }
+}
