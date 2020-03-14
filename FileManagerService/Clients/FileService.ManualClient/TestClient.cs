3f9163f407090dc7de347c124bbeff1acdcd61b0
Infarh <shmachilin@yandex.ru>
2020-03-14 18:11:43 +0300

    Пример клиента-сервера web-api

---
+++ FileManagerService/Clients/FileService.ManualClient/TestClient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using FileManagerService.Interfaces;
+
+namespace FileService.ManualClient
+{
+    class TestClient : ClientBase<IFileService>, IFileService
+    {
+        public TestClient(Binding binding, EndpointAddress endpoint) : base(binding, endpoint) { }
+
+        public DateTime GetServiceTime() => Channel.GetServiceTime();
+
+        public DriveInfo[] GetDrives() => Channel.GetDrives();
+
+        public FileInfo[] GetFiles(string Path) => Channel.GetFiles(Path);
+
+        public DirectoryInfo[] GetDirectories(string Path) => GetDirectories(Path);
+    }
+}
---
+++ FileManagerService/WebAPI/WebAPIHost/Controllers/EmployeesController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using WebAPIHost.Data;
+using WebAPIHost.Models;
+
+namespace WebAPIHost.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        public IEnumerable<Employee> Get() => TestData.Employees;
+    }
+}
