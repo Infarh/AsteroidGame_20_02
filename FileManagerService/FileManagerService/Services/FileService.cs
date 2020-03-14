cc7a2498f2dc2a47441700a9c4044f4a13a75878
Infarh <shmachilin@yandex.ru>
2020-03-14 17:12:48 +0300

    Реализация сервиса

---
+++ FileManagerService/FileManagerService/Services/FileService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.ServiceModel;
+using FileManagerService.Interfaces;
+
+namespace FileManagerService.Services
+{
+    [ServiceBehavior(
+        //InstanceContextMode = InstanceContextMode.PerSession,
+        //ConcurrencyMode = ConcurrencyMode.Multiple
+        //AutomaticSessionShutdown = false
+        IncludeExceptionDetailInFaults = true,
+        MaxItemsInObjectGraph = 100000
+        )]
+    class FileService : IFileService
+    {
+        public DriveInfo[] GetDrives() => DriveInfo.GetDrives();
+
+        public FileInfo[] GetFiles(string Path) => new DirectoryInfo(Path).GetFiles();
+
+        public DirectoryInfo[] GetDirectories(string Path) => new DirectoryInfo(Path).GetDirectories();
+    }
+}
